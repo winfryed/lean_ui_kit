014c9cf78a9867894b1fe8a204430313b4f58a01
collsko <collin.fluegel@gmail.com>
2022-07-06 18:31:13 +0200

    Comments on new Widgets

--- lib/presentation/widgets/lean_scaffold.dart
+++ lib/presentation/widgets/lean_scaffold.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 
 import '../../theming/app_theme_access.dart';
 
+/// This class represents a scaffold [Widget].
+/// A scaffold is used to implements the basic material design visual layout structure.
 class LeanScaffold extends StatelessWidget {
   const LeanScaffold({super.key, required this.body, this.backgroundColor});
 
+  /// The [Widget] that is the content of [LeanScaffold].
   final Widget? body;
+
+  /// The background color of [LeanScaffold].
   final Color? backgroundColor;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: body,backgroundColor: backgroundColor??AppThemeAccess.theme.backgroundPrimary,);
+    return Scaffold(
+      body: body,
+      backgroundColor:
+          backgroundColor ?? AppThemeAccess.theme.backgroundPrimary,
+    );
   }
 }
